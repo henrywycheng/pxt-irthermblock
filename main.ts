ccc691838bea9a2ae4fb774a4be31564157ede23
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-10 22:23:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 //% weight=0 color=#3CB371 icon="\uf2b9" block="IR Thermometer"
 
 namespace irthermoblock {
-    /* G54 TCS34725 RGBC color sensor addr 0x29 register 0x00 command 0x80 return byte */
+    /* GY-906 MLXMLX90614B IR Thermometer addr 0x5A register 0x0E return boolean */
     //% blockId="irthermoStart" block="IRThermo Start"
     //% blockGap=2 weight=89
     export function irthermoStart(): boolean {
 	pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
 	pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
-	pins.i2cWriteNumber(90,46,NumberFormat.UInt8LE,false)
+	pins.i2cWriteNumber(90,46,NumberFormat.UInt8LE,true)
+	if (pins.i2cReadNumber(90,NumberFormat.UInt8LE,false) != 90) {
+		return false
+	}
 	return true
     }
 
