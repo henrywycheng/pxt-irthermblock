96b084f30fe6838396d3218cd78824de1e87e9b3
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-10 23:03:57 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -21,8 +21,8 @@ namespace irthermoblock {
 	pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
 	pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
 	pins.i2cWriteNumber(90,6,NumberFormat.UInt8LE,true)
-	var Ta = pins.i2cReadNumber(90,NumberFormat.UInt16LE,false)
-	Ta = Ta * 2 / 100 - 273.15
+	let Ta = pins.i2cReadNumber(90,NumberFormat.UInt16LE,false)
+	/*Ta = Ta * 2 / 100 - 273.15*/
 	return Ta
     }
 
