5ffc823509cca5144bd7601a9a07a6b61f5f4bc5
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-10 21:31:50 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace irthermoblock {
     /* G54 TCS34725 RGBC color sensor addr 0x29 register 0x00 command 0x80 return byte */
     //% blockId="irthermoStart" block="IRThermo Start"
     //% blockGap=2 weight=89
-    export function irthermoStart() : boolean {
+    export function irthermoStart(): boolean {
 	pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
 	pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
 	pins.i2cWriteNumber(90,46,NumberFormat.UInt8LE,false)
