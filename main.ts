05161721d2ec8b05b824baa8a57dca5e17514bb9
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-11 21:56:24 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -25,5 +25,28 @@ namespace irthermo {
         return Math.round(Ta * 0.02 - 273.15)
     }
 
+
+    /* GY-906 MLX90614B IR Thermometer addr 0x5A register 0x07 return object1 temperature */
+    //% blockId="irthermoTo1" block="IRThermo get To1"
+    //% blockGap=2 weight=88
+    export function irthermoTo1(): number {
+	pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
+	pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
+	pins.i2cWriteNumber(90,6,NumberFormat.UInt8LE,true)
+	let To1 = pins.i2cReadNumber(90, NumberFormat.UInt16LE, false)
+        return Math.round(To1 * 0.02 - 273.15)
+    }
+
+    /* GY-906 MLX90614B IR Thermometer addr 0x5A register 0x08 return object2 temperature */
+    //% blockId="irthermoTo2" block="IRThermo get To2"
+    //% blockGap=2 weight=88
+    export function irthermoTo2(): number {
+	pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
+	pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
+	pins.i2cWriteNumber(90,6,NumberFormat.UInt8LE,true)
+	let To1 = pins.i2cReadNumber(90, NumberFormat.UInt16LE, false)
+        return Math.round(To2 * 0.02 - 273.15)
+    }
+
 }
 
