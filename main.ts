b2d7f31546e3dd32cea8e29a63c700d7c513dae0
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-11 21:40:24 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 
 namespace irthermo {
     /* GY-906 MLX90614B IR Thermometer addr 0x5A register 0x0E return boolean */
-    //% blockId="irthermoStart" block="IR Thermo Start"
+    //% blockId="irthermoStart" block="IRThermo Start"
     //% blockGap=2 weight=89
     export function irthermoStart() {
 	pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
@@ -15,7 +15,7 @@ namespace irthermo {
     }
 
     /* GY-906 MLX90614B IR Thermometer addr 0x5A register 0x06 return ambient temperature */
-    //% blockId="irthermoTa" block="IR Thermo get Ta"
+    //% blockId="irthermoTa" block="IRThermo get Ta"
     //% blockGap=2 weight=88
     export function irthermoTa(): number {
 	pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
