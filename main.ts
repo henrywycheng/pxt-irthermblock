de6a2156ce621968b89ac4b5d168c6b56ccc5fad
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-11 18:16:02 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=0 color=#3CB371 icon="\uf2b9" block="IR Thermometer"
+//% weight=0 color=#3CB371 icon="\uf2b9" block="IRThermometer"
 
 namespace irthermo {
     /* G54 TCS34725 RGBC color sensor addr 0x29 register 0x00 command 0x80 return byte */
